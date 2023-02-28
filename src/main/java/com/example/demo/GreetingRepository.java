cc075ebcaf61b26637b8b6c1798627d434f762b1
Mihai <mihai_j_a@yahoo.com>
2023-02-28 18:14:40 +0200

    modified repository,service and serviceImpl

--- src/main/java/com/example/demo/GreetingRepository.java
+++ src/main/java/com/example/demo/GreetingRepository.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface GreetingRepository extends CrudRepository<Greeting, Long> {
-
+    Set<Greeting> findAll();
 }
