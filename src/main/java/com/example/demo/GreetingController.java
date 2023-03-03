3eb1a6089acf5c3070bac2b03c60e23c94c77287
Mihai <mihai_j_a@yahoo.com>
2023-03-03 20:21:45 +0200

    long PathVariable

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -46,8 +46,8 @@ public class GreetingController {
     }
 
     @GetMapping("/greeting/{id}/view")
-    public String showGreeting(@PathVariable String id, Model model){
-        Optional<Greeting> greetingOptional = greetingService.findById(Long.valueOf(id));
+    public String showGreeting(@PathVariable Long id, Model model){
+        Optional<Greeting> greetingOptional = greetingService.findById(id);
         log.info(String.valueOf(greetingOptional.get().getId()));
 
         if (!greetingOptional.isPresent()){
